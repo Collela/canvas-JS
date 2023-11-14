9fd7c7fc0d6a88b1c861835e3372688493c560f6
Dagmar Collela <dagmarcollela@gmail.com>
2023-11-14 12:26:29 -0300

    Criando Script
    ## Adição de funcionalidades de desenho
    
    Iniciei o script capturando as propriedades do input e do elemento canvas. Também defini alguns valores iniciais, como a cor inicial da caneta (preta), a posição do mouse (inicialmente em 0) e a propriedade canDraw como falsa. Além disso, defini a variável `ctx` (contexto), responsável por capturar as propriedades 2D do quadro.
    
    A primeira função verifica se uma cor foi selecionada no input de cores. Em caso positivo, a cor padrão da caneta é alterada para a selecionada. O evento `color.onchange` é utilizado para monitorar as mudanças no input de cor.
    
    A função `mouseDownEvent` indica que o mouse está sendo pressionado sobre o quadro, tornando a propriedade canDraw verdadeira. Esta função também captura a posição inicial do mouse no quadro. À medida que o mouse se move sobre o quadro, a função `mouseMoveEvent` é chamada, que, por sua vez, aciona a função `draw`.
    
    A função `draw` recebe as coordenadas do quadro e define as propriedades do traço do desenho, como tamanho, formato e posição. Isso ocorre conforme o movimento do mouse.
    
    Se o mouse for solto, a função `mouseUpEvent` é chamada para definir a propriedade canDraw como falsa novamente, indicando o término do desenho.

--- script.js
+++ script.js
@@ -2,11 +2,53 @@ const color = document.querySelector('input'); // captura as propriedades do inp
 let screen = document.querySelector('canvas'); // ''           ''         do elemento canvas
 
 // definindo propriedades inicial
-let defaultcolor = 'black';
+let defaultColor = 'black';
 let canDraw = false;
 let mouseX = 0;
 let mouseY = 0;
 
-// definindo propriedade da variavel ctx para capturar as propriedades 2D
+// definindo propriedade da variavel ctx para captura 2D
 let ctx = screen.getContext('2d');
 
+color.onchange = () => defaultColor = color.value;
+
+screen.addEventListener('mousedown', mouseDownEvent);
+screen.addEventListener('mousemove', mouseMoveEvent);
+screen.addEventListener('mouseup', mouseUpEvent);
+
+function mouseDownEvent(e){
+    canDraw = true;
+    mouseX = e.pageX - screen.offsetLeft;
+    mouseY = e.pageY - screen.offsetTop;
+};
+function mouseMoveEvent(e){
+    if(canDraw){
+        draw(e.pageX, e.pageY);
+    }
+};
+
+function mouseUpEvent(e){
+    canDraw = false;
+};
+
+function draw(x, y){
+    let pointX = x - screen.offsetLeft;
+    let pointY = y - screen.offsetTop;
+
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.lineJoin = "round";
+    ctx.moveTo(mouseX, mouseY);
+    ctx.lineTo(pointX, pointY);
+    ctx.closePath();
+    ctx.strokeStyle = defaultColor;
+    ctx.stroke();
+
+    mouseX = pointX;
+    mouseY = pointY;
+};
+
+function clearBoard(){
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+};
